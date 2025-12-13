9165422f75fd591499e0417294ffbb6bad4f026d
Erick <erickbs2006@gmail.com>
2025-12-13 12:13:52 -0300

    abrir dashboard

--- front/js/dashboard.js
+++ front/js/dashboard.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Verifica se existe uma conta autenticada
-    const contaAtiva = localStorage.getItem("contaAtiva");
+    // const contaAtiva = localStorage.getItem("contaAtiva");
 
-    // Se não houver conta ativa, redireciona para o login
-    if (!contaAtiva) {
-        window.location.href = "index.html";
-        return; // Para evitar que o código continue executando
-    }
+    // // Se não houver conta ativa, redireciona para o login
+    // if (!contaAtiva) {
+    //     window.location.href = "index.html";
+    //     return; // Para evitar que o código continue executando
+    // }
 
     // Botão para ir à transferência
     document.getElementById("btn-transferir")
